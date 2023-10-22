25ec668b14792f5ddee1a15c68c2db0890a384c0
pingthSK <141994287+pingthSK@users.noreply.github.com>
2023-10-22 21:35:32 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,6 +17,20 @@ function myFunction(){
     d.set('3737', 'goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog2481goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog1442goggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggoggog')
     d.set("2481", 'epic super SuS video (Legendey rank )');
     d.set("1442", 'the next conclusion ?????');
+    d.set("Halloween2023", 'h12');
+    d.set("h12", 'It’s Halloween! But guess what?, It’s kinda dark outside tho\ngo outside:h378\nstay in:h245');
+    d.set("h378", "You came outside, the door slam bang!\n go trick or treat🍬🤲:h112\n go jumpscare ppl lol ez💀👻😬: ");
+    d.set("h112", "You knocked your neighbor’s door but she gave you nothing\n trick or treat again: h999\n go inside the door: h123")
+    d.set("h999", "The END! here’s some candies btw &youtube candies link$")
+    d.set("h123”, 'omg its the jeck o lanterns!!1!!!1 RUNnnNn\n Run and swear to jeck: h638\n Say “Let’s be friend jeckky”:h395')
+    d.set("h683", 'Th result is DEATHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH BUt the next day she forgive you and let you live ayyayayy')
+    d.set("h395", "Let’s bee Friendd UwU  ahhh ahhhh ahhhhhh ahhhh $corn hub$")
+    d.set("h245", "You close the door slowly… you turn your back 180 degrees then you saw a craved pumkin laying on the table (type h84 for more clue)")
+    d.set("h84", '$image saying “destroy it before it destroy you (with the pic of scary craved pumkin with flash light inside$ \n h42: Run real quick!!\n Eat it munch munch: h290')
+    d.set("h42", 'Jump scare $Song-Five night at Freddy')
+    d.set("h290", 'Nom nom nom, Mmmmm, So delicious! Crunchy sweet umami! Ima go find more pumkiinnn! $Link below mail ping to get free pumkin plushie or diy product')
+
+
     let test = parseInt(code);
     function linkopen(x){
     const link = document.getElementById('link');
